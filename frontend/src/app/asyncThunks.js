11761cb9387ea5817db86f570bce054418cd53da
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-10-04 21:56:14 +0530

    Updated backend url

--- frontend/src/app/asyncThunks.js
+++ frontend/src/app/asyncThunks.js
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const APIURL = "http://localhost:5000/api";
+// const APIURL = "http://localhost:5000/api/";
+const APIURL = "https://client-connect.onrender.com/";
 
 export const login = createAsyncThunk("auth/login", async (credentials) => {
   const response = await axios.post(`${APIURL}users/login`, credentials);
